376c62ca73cdaa37e6b9e6b489880ecaa9558e38
Eugene Shevchenko <eugeneShevchenko0@gmail.com>
2024-01-25 22:18:36 +0500

    Markdown with Markdig lib

--- learn/Models/GameDeveloper.cs
+++ learn/Models/GameDeveloper.cs
@@ -5,5 +5,7 @@
 		public int Id { get; set; }
 		public string Name { get; set; } = string.Empty;
 		public string Image {  get; set; } = string.Empty;
-	}
+
+		public string StudioDescription { get; set; } = string.Empty;
+    }
 }
